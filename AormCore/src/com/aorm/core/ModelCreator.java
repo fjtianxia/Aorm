7272e54563295a2cecc7a20944e040611cebcdcc
fengjian <673592063@qq.com>
2011-09-04 19:13:36 +0800

    init the ModelCreator's some method...

---
+++ AormCore/src/com/aorm/core/ModelCreator.java
@@ -0,0 +1,26 @@
+package com.aorm.core;
+
+import java.util.List;
+
+public class ModelCreator<T> {
+    
+    public T get(){
+        return null;
+    }
+    
+    public List<T> list(){
+        return null;
+    }
+    
+    public boolean insert(){
+        return false;
+    }
+    
+    public boolean delete(){
+        return false;
+    }
+    
+    public int size(){
+        return 0;
+    }
+}
--- README.txt
+++ README.txt
@@ -4,6 +4,10 @@ Email:673592063@qq.com
 我们的目标是：
 Person person = new Person();
 person.id = 5;
-person.list();
-person.insert();
-person.delete();
+
+ModelCreator<Person> creator = new ModelCreator<Person>();
+creator.get();
+creator.list();
+creator.insert();
+creator.delete();
+creator.size();
